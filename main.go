229e524a80ff2b94c262420ee9e0c8aa4f5c8a18
Brian Goff <cpuguy83@gmail.com>
2020-10-19 16:29:20 -0700

    log and exit instead of panic
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>

--- main.go
+++ main.go
@@ -4,49 +4,37 @@ import (
 	"bufio"
 	"bytes"
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
+	"runtime"
 	"syscall"
 
 	"github.com/containerd/containerd/remotes/docker"
-	"github.com/opencontainers/go-digest"
 	v1 "github.com/opencontainers/image-spec/specs-go/v1"
 	"golang.org/x/crypto/ssh/terminal"
 )
 
 func main() {
-	var (
-		dgst digest.Digest
-		data []byte
-		err  error
-	)
-
-	dgst, data, err = FromFile(os.Args[2])
+	if len(os.Args) < 3 {
+		errOut(errors.New("usage: " + filepath.Base(os.Args[0]+" <repo> <manifest.json>")))
+	}
+	dgst, data, err := FromFile(os.Args[2])
 	if err != nil {
-		panic(err)
+		errOut(err)
+	}
+
+	var credsFunc func(string) (string, string, error)
+	if terminal.IsTerminal(syscall.Stdin) {
+		credsFunc = terminalCreds
 	}
 
 	resolver := docker.NewResolver(docker.ResolverOptions{
 		Hosts: docker.ConfigureDefaultRegistries(
 			docker.WithAuthorizer(docker.NewDockerAuthorizer(
-				docker.WithAuthCreds(func(host string) (string, string, error) {
-					fmt.Fprintf(os.Stderr, "Username: ")
-
-					stdin := bufio.NewReader(os.Stdin)
-					user, _, err := stdin.ReadLine()
-					if err != nil {
-						return "", "", err
-					}
-
-					fmt.Fprintf(os.Stderr, "Password: ")
-					pass, err := terminal.ReadPassword(int(syscall.Stdin))
-					if err != nil {
-						return "", "", err
-					}
-
-					return string(user), string(pass), nil
-				}),
+				docker.WithAuthCreds(credsFunc),
 			)),
 		),
 	})
@@ -56,7 +44,7 @@ func main() {
 	ref := os.Args[1]
 	pusher, err := resolver.Pusher(ctx, ref)
 	if err != nil {
-		panic(err)
+		errOut(err)
 	}
 
 	desc := v1.Descriptor{
@@ -64,9 +52,42 @@ func main() {
 		Digest:    dgst,
 		Size:      int64(len(data)),
 	}
-	w, err := pusher.Push(ctx, desc)
 
+	w, err := pusher.Push(ctx, desc)
 	if _, err := io.Copy(w, bytes.NewReader(data)); err != nil {
-		panic(err)
+		errOut(err)
 	}
+}
+
+func errOut(err error) {
+	if err == nil {
+		return
+	}
+
+	_, f, l, _ := runtime.Caller(1)
+	if wd, err := os.Getwd(); err == nil {
+		if rel, err := filepath.Rel(filepath.Dir(f), wd); err == nil {
+			f = filepath.Join(rel, filepath.Base(f))
+		}
+	}
+	fmt.Fprintf(os.Stderr, "%s:%d %s\n", f, l, err.Error())
+	os.Exit(1)
+}
+
+func terminalCreds(host string) (string, string, error) {
+	fmt.Fprintf(os.Stderr, "Username: ")
+
+	stdin := bufio.NewReader(os.Stdin)
+	user, _, err := stdin.ReadLine()
+	if err != nil {
+		return "", "", err
+	}
+
+	fmt.Fprintf(os.Stderr, "Password: ")
+	pass, err := terminal.ReadPassword(int(syscall.Stdin))
+	if err != nil {
+		return "", "", err
+	}
+
+	return string(user), string(pass), nil
 }
