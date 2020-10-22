6a6dda2d270a419195012e7401377a9090d2dfdb
Brian Goff <cpuguy83@gmail.com>
2020-10-21 21:36:36 -0700

    Add support for fetching from the registry
    This kinda-sorta works.
    I've found some blobs don't want to fetch.
    
    As an exmaple:
    
    ```terminal
    $ zapp docker.io/library/busybox:latest
    <yields manifest list>
    $ zapp docker.io/library/busybox:latest <manifest sha>
    ```
    
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>

--- main.go
+++ main.go
@@ -12,6 +12,9 @@ import (
 
 	"github.com/containerd/containerd/errdefs"
 	"github.com/containerd/containerd/log"
+	"github.com/containerd/containerd/remotes"
+	"github.com/opencontainers/go-digest"
+	v1 "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/crypto/ssh/terminal"
 )
@@ -35,7 +38,7 @@ func main() {
 		ctx = log.WithLogger(ctx, l)
 	}
 
-	if len(flag.Args()) < 2 {
+	if len(flag.Args()) < 1 {
 		errOut(errors.New("usage: " + filepath.Base(os.Args[0]+" <repo> <file> [<media type>]")))
 	}
 
@@ -43,18 +46,94 @@ func main() {
 	fileName := flag.Arg(1)
 	mt := flag.Arg(2)
 
-	f, desc, err := FromFile(fileName, mt)
-	if err != nil {
-		errOut(err)
-	}
-	defer f.Close()
-
 	var credsFunc func(string) (string, string, error)
 	if terminal.IsTerminal(syscall.Stdin) {
 		credsFunc = terminalCreds
 	}
 
 	resolver := getResolver(credsFunc)
+
+	if fileName == "" {
+		// no file or sha is given, assume this is just a manifest request
+		if err := fetch(ctx, resolver, ref, ""); err != nil {
+			errOut(err)
+		}
+		return
+	}
+
+	f, desc, err := FromFile(fileName, mt)
+	switch {
+	case err == nil:
+		defer f.Close()
+		if err := push(ctx, resolver, ref, desc, f); err != nil {
+			errOut(err)
+		}
+		return
+	case os.IsNotExist(err):
+		dgst, err2 := digest.Parse(fileName)
+		if err2 != nil {
+			errOut(err)
+		}
+		if err := fetch(ctx, resolver, ref, dgst); err != nil {
+			errOut(err)
+		}
+		return
+	}
+}
+
+func fetch(ctx context.Context, resolver remotes.Resolver, ref string, dgst digest.Digest) (retErr error) {
+	defer func() {
+		if retErr != nil {
+			retErr = fmt.Errorf("fetch: %w", retErr)
+		}
+	}()
+	fetcher, err := resolver.Fetcher(ctx, ref)
+	if err != nil {
+		return err
+	}
+
+	_, desc, err := resolver.Resolve(ctx, ref)
+	if err != nil {
+		return fmt.Errorf("error resolving reference: %w", err)
+	}
+
+	if dgst != "" {
+		desc.Digest = dgst
+		desc.MediaType = ""
+	}
+
+	rdr, err := fetcher.Fetch(ctx, desc)
+	if err != nil {
+		return fmt.Errorf("error fetching content: %w", err)
+	}
+	defer rdr.Close()
+
+	h := desc.Digest.Algorithm().Digester().Hash()
+	r := io.TeeReader(rdr, h)
+
+	buf := make([]byte, 1<<20)
+
+	_, err = io.CopyBuffer(os.Stdout, r, buf)
+	if err != nil {
+		return fmt.Errorf("error reading content: %w", err)
+	}
+
+	if err := rdr.Close(); err != nil {
+		return err
+	}
+
+	if desc.Digest != digest.NewDigest(digest.Canonical, h) {
+		return errors.New("digest mistmatch")
+	}
+	return nil
+}
+
+func push(ctx context.Context, resolver remotes.Resolver, ref string, desc v1.Descriptor, f io.Reader) (retErr error) {
+	defer func() {
+		if retErr != nil {
+			retErr = fmt.Errorf("push: %w", retErr)
+		}
+	}()
 
 	fmt.Println("Type:", desc.MediaType)
 	fmt.Println("Size:", desc.Size)
@@ -68,17 +147,18 @@ func main() {
 	w, err := pusher.Push(ctx, desc)
 	if err != nil {
 		if errdefs.IsAlreadyExists(err) {
-			return
+			return nil
 		}
-		errOut(err)
+		return err
 	}
 
 	buf := make([]byte, 1<<20)
 	if _, err := io.CopyBuffer(w, f, buf); err != nil {
-		errOut(fmt.Errorf("error copying to remote: %w", err))
+		return fmt.Errorf("error copying to remote: %w", err)
 	}
 
 	if err := w.Commit(ctx, desc.Size, desc.Digest); err != nil {
-		errOut(err)
+		return err
 	}
+	return nil
 }
